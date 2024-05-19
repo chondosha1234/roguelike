52714a3d3c6d1bf0d9dc42ece17474f0de486f3b
chondosha1234 <jmiller61193@gmail.com>
2024-05-19 18:32:54 -0400

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+
+use tcod::colors::*;
+use tcod::console::*;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const LIMIT_FPS: i32 = 20; // 20 fps maximum
+
+struct Tcod {
+    root: Root,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
