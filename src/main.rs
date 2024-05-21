3ee5fd2ca16567c0326c758980ce418dc4850022
chondosha1234 <jmiller61193@gmail.com>
2024-05-20 20:33:26 -0400

    added map generation and fov

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-
+use std::cmp;
+use rand::Rng;
 use tcod::colors::*;
 use tcod::console::*;
+use tcod::map::{FovAlgorithm, Map as FovMap};  // rename tcod Map type as FovMap
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
+const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;   //default algorithm 
+const FOV_LIGHT_WALLS: bool = true;  // light walls or not
+const TORCH_RADIUS: i32 = 10;
 
 const COLOR_DARK_WALL: Color = Color { r:0, g: 0, b: 100 };
+const COLOR_LIGHT_WALL: Color = Color { r: 130, g: 110, b: 50 };
 const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
-
+const COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b: 50 };
 
 const LIMIT_FPS: i32 = 20; // 20 fps maximum
 
@@ -17,12 +26,14 @@ const LIMIT_FPS: i32 = 20; // 20 fps maximum
 struct Tcod {
     root: Root,
     con: Offscreen,
+    fov: FovMap,
 }
 
 // struct of map tile and properties
 #[derive(Clone, Copy, Debug)]
 struct Tile {
     blocked: bool,
+    explored: bool,
     block_sight: bool,
 }
 
@@ -30,6 +41,7 @@ impl Tile {
     pub fn empty() -> Self {
         Tile {
             blocked: false,
+            explored: false,
             block_sight: false,
         }
     }
@@ -37,8 +49,46 @@ impl Tile {
     pub fn wall() -> Self {
         Tile {
             blocked: true,
+            explored: false,
             block_sight: true,
         }
+    }
+}
+
+// rectangle on map representing a room, has coordinates of top left and bottom right
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    // create new rectangle with top left and dimensions 
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x+w,
+            y2: y+h,
+        }
+    }
+
+    // get the center of a rectangle room -- used for start of tunnel 
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    // function to check if rooms are overlapping 
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        // return true if room intersects with another 
+        (self.x1 <= other.x2)
+            && (self.x2 >= other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
     }
 }
 
@@ -78,34 +128,141 @@ impl Object {
 }
 
 
+
+
 // function to create map with vec! macro 
-fn make_map() -> Map {
-    // fill map with unblocked tiles
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+fn make_map(player: &mut Object) -> Map {
+    // fill map with wall tiles
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    
+    let mut rooms = vec![];
+
+    for _ in 0..MAX_ROOMS {
+        // random width and height 
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        // random position without going out of bounds of map 
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+
+        let new_room = Rect::new(x, y, w, h);
+
+        // check intersections with other rooms using closure 
+        // any() will run on every element aborts if it encounters false 
+        let failed = rooms.iter().any(|other_room| new_room.intersects_with(other_room)); 
+        
+        // if it is valid spot then create room
+        if !failed {
+            // add room by drawing the map tiles 
+            create_room(new_room, &mut map);
+            
+            // get center coordinates of room
+            let (new_x, new_y) = new_room.center();
+
+            // put player in room if its first room
+            if rooms.is_empty() {
+                player.x = new_x;
+                player.y = new_y;
+
+            } else {
+                // else need to connect this room to previous room 
+                // get previous room center 
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                // flip coin -- get random bool value 
+                if rand::random() {
+                    // first do horizontal then vertical
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    // first do vertical then horizontal 
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+            // add room to list 
+            rooms.push(new_room);
+        }
+    }
 
     map   // return the map 
 }
 
+// function to add room to map 
+fn create_room(room: Rect, map: &mut Map) {
+    // go through tiles in rectangle and make them passable
+    // loops exclude first and last to make walls
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+// function to create horizontal tunnels 
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    // min and max used if x1 > x2
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+// function to create vertical tunnels 
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    // min and max used if y1 > y2
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
 // function to draw all objects and map 
-fn render_all(tcod: &mut Tcod, game : &Game, objects: &[Object]) {
-    // draw all objects in list 
-    for object in objects {
-        object.draw(&mut tcod.con);
+fn render_all(tcod: &mut Tcod, game: &mut Game, objects: &[Object], fov_recompute: bool) {
+    
+    // recompute fov if needed
+    if fov_recompute {
+        let player = &objects[0];
+        tcod.fov.compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
     }
 
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
+            // check if this position is in fov
+            let visible = tcod.fov.is_in_fov(x, y);
+            
+            // wall is bool for block sight 
             let wall = game.map[x as usize][y as usize].block_sight;
-            if wall {
-                tcod.con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-            } else {
-                tcod.con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            
+            // set color based on fov and tile type 
+            let color = match (visible, wall) {
+                //out side field of view
+                (false, true) => COLOR_DARK_WALL,
+                (false, false) => COLOR_DARK_GROUND,
+                // inside fov 
+                (true, true) => COLOR_LIGHT_WALL,
+                (true, false) => COLOR_LIGHT_GROUND,
+            };
+
+            let explored = &mut game.map[x as usize][y as usize].explored;
+            if visible {
+                // if it is visible set explore 
+                *explored = true;
+            }
+
+            if *explored {
+                //show explored tiles only
+                tcod.con.set_char_background(x, y, color, BackgroundFlag::Set);
             }
         }
     }
- 
+    
+    // draw all objects in list 
+    for object in objects {
+        if tcod.fov.is_in_fov(object.x, object.y) {
+            object.draw(&mut tcod.con);
+        }
+    }
+
+
     blit(
         &tcod.con,
         (0, 0),
@@ -157,24 +314,47 @@ fn main() {
         .title("Rust/libtcod tutorial")
         .init();
 
-    let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
-
-    let mut tcod = Tcod { root, con };
+    // initialize console and fovmap in struct init 
+    let mut tcod = Tcod { 
+        root, 
+        con: Offscreen::new(MAP_WIDTH, MAP_HEIGHT),
+        fov: FovMap::new(MAP_WIDTH, MAP_HEIGHT), 
+    };
 
     tcod::system::set_fps(LIMIT_FPS);
-
-    let game = Game { map: make_map(), };
     
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
-    let npc = Object::new(SCREEN_WIDTH / 2-5, SCREEN_HEIGHT / 2, '@', YELLOW);
-
-    let mut objects = [player, npc];
+    // create player object and object list 
+    let player = Object::new(0, 0, '@', WHITE);
+    let mut objects = [player];
     
+    let mut game = Game {
+        // generate map 
+        map: make_map(&mut objects[0]),
+    };
+    
+    // populate FOV map according to generated map 
+    for y in 0..MAP_HEIGHT{
+        for x in 0..MAP_WIDTH {
+            // tcod needs opposite values from what we set, so use negation
+            tcod.fov.set(
+                x,
+                y,
+                !game.map[x as usize][y as usize].block_sight,
+                !game.map[x as usize][y as usize].blocked,
+            );
+        }
+    }
+
+    // force FOV "recompute" first time through game loop because invalid position
+    let previous_player_position = (-1, -1);
+
     // main game loop 
     while !tcod.root.window_closed() {
         tcod.con.clear();
         
-        render_all(&mut tcod, &game, &objects);
+        // recompute if player has moved
+        let fov_recompute = previous_player_position != (objects[0].x, objects[0].y);
+        render_all(&mut tcod, &mut game, &objects, fov_recompute);
 
         tcod.root.flush();
         
